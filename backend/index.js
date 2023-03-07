ffff318ed7755e2f8eac68c0bc355b5df99b333c
Jason Armenta <j_armenta@mail.fhsu.edu>
2023-03-07 15:49:02 -0600

    Created Frameworks for Back and Front

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express from "express"
+import mysql from "mysql"
+import cors from "cors"
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+//Database Connection 
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database: "blobdata"
+})
+
+//Initial Check to see if I can connect to the backend
+app.get("/", (req,res)=>{
+    res.json("Welcome to the backend!")
+})
+
+//Access to SQL Data
+app.get("/test", (req, res)=>{
+    const q = "SELECT trace_data FROM test"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+
+        const blobData = data[0].trace_data;
+        
+        const newIntegers = [];
+        for (let i = 0; i < blobData.length; i += 4){
+            newIntegers.push(blobData.readInt32BE(i) / 1000);
+        }
+        res.json(newIntegers);
+    })
+})
+
+//Checks for successful connection on port 8800
+app.listen(8800, ()=>{
+    console.log("Connected to backend")
+})
